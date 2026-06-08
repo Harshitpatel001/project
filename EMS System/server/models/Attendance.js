d1bf4785aafe323c9069b4e467ac5130f2a6f521
Harshit <2k23.cs2313767@gmail.com>
2026-06-08 22:14:51 +0530

    API created for leave application

--- EMS System/server/models/Attendance.js
+++ EMS System/server/models/Attendance.js
@@ -12,7 +12,7 @@ const AttendanceSchema = new mongoose.Schema({
         default:null}
 }, { timestamps: true })
 
-attendanceSchema.index({employeeId: 1,date: 1},{unique:true})
+AttendanceSchema.index({employeeId: 1,date: 1},{unique:true})
 
 const Attendance = mongoose.models.Attendance || mongoose.model("Attendance",
     AttendanceSchema)
