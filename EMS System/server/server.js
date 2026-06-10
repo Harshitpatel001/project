68be04c2c162d23658e7c623bb30ec64c9a503b7
Harshit <2k23.cs2313767@gmail.com>
2026-06-10 07:10:23 +0530

    Backend completed

--- EMS System/server/server.js
+++ EMS System/server/server.js
@@ -13,6 +13,9 @@ import attendanceRouter from './routes/attendanceRoutes.js';
 import payslipRouter from './routes/payslipsRoutes.js';
 import dashboardRouter from './routes/dashboardRoutes.js';
 
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
+
 const app = express()
 const PORT = process.env.PORT || 4000;
 
@@ -30,6 +33,8 @@ app.use("/api/attendance",attendanceRouter)
 app.use("/api/leave",leaveRouter)
 app.use("/api/payslips",payslipRouter)
 app.use("/api/dashboard",dashboardRouter)
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 await connectDB();
 app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))
